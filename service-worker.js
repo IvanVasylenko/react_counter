c6a47f938b87e524856af2b94037a3eadd7cc6c4
”Ivan <ivasylenko@ukr.net>
2021-05-24 18:22:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.cfad367fc9b1221a8e9bd9c2d58cb537.js"
 );
 
 self.addEventListener('message', (event) => {
